4f927b557d5b65bcf0339263dc317522df78e38c
Emerson Matias <emersonsmatthias@gmail.com>
2023-03-26 19:24:51 -0300

    fix: spoken password

--- src/pages/ScreenType1/functionsSideBar.tsx
+++ src/pages/ScreenType1/functionsSideBar.tsx
@@ -1,30 +1,47 @@
-export function passwordConfigs(event: React.KeyboardEvent<HTMLDivElement>, password: number, setPassword: React.Dispatch<React.SetStateAction<number>>,passworDigit: string, setPasswordDigit: React.Dispatch<React.SetStateAction<string>>) {
-    if(event.key === "Enter"){
-        
-        if(passworDigit.length){
+export function passwordConfigs(event: React.KeyboardEvent<HTMLDivElement>, password: number, setPassword: React.Dispatch<React.SetStateAction<number>>, passworDigit: string, setPasswordDigit: React.Dispatch<React.SetStateAction<string>>) {
+
+    if (event.key === "Enter") {
+        console.log(passworDigit, password)
+
+        if (passworDigit.length) {
+            console.log("AAA")
             setPasswordDigit("")
+          
+
+            if ('speechSynthesis' in window) {
+                const utterance = new SpeechSynthesisUtterance();
+                utterance.text = `Senha ${Number(passworDigit)}. Repetindo, senha ${passworDigit}`;
+                utterance.lang = 'pt-BR';
+                utterance.rate = 1;
+                utterance.pitch = 1;
+                utterance.volume = 1;
+                speechSynthesis.speak(utterance);
+            }
+
             return setPassword(Number(passworDigit))
+        } else {
+            setPassword(password + 1)
+
+            if ('speechSynthesis' in window) {
+                const utterance = new SpeechSynthesisUtterance();
+                utterance.text = `Senha ${password + 1}. Repetindo, senha ${password + 1}`;
+                utterance.lang = 'pt-BR';
+                utterance.rate = 1;
+                utterance.pitch = 1;
+                utterance.volume = 1;
+                speechSynthesis.speak(utterance);
+            }
         }
 
-        setPassword(password + 1)
 
-        if ('speechSynthesis' in window) {
-            const utterance = new SpeechSynthesisUtterance();
-            utterance.text = `Senha ${password + 1}. Repetindo, senha ${password + 1}`;
-            utterance.lang = 'pt-BR';
-            utterance.rate = 1;
-            utterance.pitch = 1;
-            utterance.volume = 1;
-            speechSynthesis.speak(utterance);
-        }
     }
 
-    if(!isNaN(Number(event.key))){
-        setPasswordDigit(passworDigit+event.key)
+    if (!isNaN(Number(event.key))) {
+        setPasswordDigit(passworDigit + event.key)
         console.log(passworDigit)
     }
 
-    if(event.key === "Escape"){
+    if (event.key === "Escape") {
         setPasswordDigit("")
     }
 
